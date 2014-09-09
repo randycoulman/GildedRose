134b3d266bca5f532599e2a4040c89d6bd98e570
xp <rcoulman@gmail.com>
2014-09-08 19:39:48 -0700

    Extract body of update to ItemWrapper

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -2,29 +2,8 @@ require './item.rb'
 require 'delegate'
 
 class ItemWrapper < SimpleDelegator
-end
-
-class GildedRose
-
-  @items = []
-
-  def initialize
-    @items = []
-    @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
-    @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
-    @items << Item.new("Conjured Mana Cake", 3, 6)
-  end
-
-  def update_quality
-    @items.each do |item|
-      update(ItemWrapper.new(item))
-    end
-  end
-
-  def update(item)
+  def update
+    item = self
     if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
       if item.quality > 0
         if item.name != "Sulfuras, Hand of Ragnaros"
@@ -68,6 +47,31 @@ class GildedRose
         end
       end
     end
+  end
+end
+
+class GildedRose
+
+  @items = []
+
+  def initialize
+    @items = []
+    @items << Item.new("+5 Dexterity Vest", 10, 20)
+    @items << Item.new("Aged Brie", 2, 0)
+    @items << Item.new("Elixir of the Mongoose", 5, 7)
+    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
+    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    @items << Item.new("Conjured Mana Cake", 3, 6)
+  end
+
+  def update_quality
+    @items.each do |item|
+      update(ItemWrapper.new(item))
+    end
+  end
+
+  def update(item)
+    item.update
   end
 
 end
